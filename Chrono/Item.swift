40948d5af419f4a8d1ad640d1d5485c60907492e
neon443 <69979447+neon443@users.noreply.github.com>
2024-12-01 19:12:31 +0000

    create .xcodeproj

---
+++ Chrono/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Chrono
+//
+//  Created by Nihaal Sharma on 01/12/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ChronoTests/ChronoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChronoTests.swift
+//  ChronoTests
+//
+//  Created by Nihaal Sharma on 01/12/2024.
+//
+
+import Testing
+
+struct ChronoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
